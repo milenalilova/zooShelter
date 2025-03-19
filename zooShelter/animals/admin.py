c74d41f9f9594fc64530a2be5afbf2f4a2f4c818
milenalilova <milenalilova78@gmail.com>
2025-03-19 17:19:26 -0300

    Registered models in animals admin

--- zooShelter/animals/admin.py
+++ zooShelter/animals/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from zooShelter.animals.models import AnimalCategory, Animal, AnimalImage
+
+
+@admin.register(AnimalCategory)
+class AnimalCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = ('personal_name', 'species')
+
+
+@admin.register(AnimalImage)
+class AnimalImageAdmin(admin.ModelAdmin):
+    list_display = ('animal', 'is_primary')
