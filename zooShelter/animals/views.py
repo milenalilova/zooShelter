4d9c265dafeb6a9452f7d41151e3f92cd8b4d2c2
milenalilova <milenalilova78@gmail.com>
2025-03-22 18:12:24 -0300

    Created animals views

--- zooShelter/animals/views.py
+++ zooShelter/animals/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
 
-# Create your views here.
+
+class AnimalCreateView(CreateView):
+    pass
+
+
+class AnimalDetailsView(DetailView):
+    pass
+
+
+class AnimalListView(ListView):
+    pass
+
+
+class AnimalEditView(UpdateView):
+    pass
+
+
+class AnimalDeleteView(DeleteView):
+    pass
